97dacc47f7ed9f8dfc25aed63d83892765171b9d
azamchaiforoush <84159966+azamchaiforoush@users.noreply.github.com>
2021-07-18 15:15:08 -0700

    StateCapital in NetBeans

---
+++ src/main/java/StateCapitals.java
@@ -0,0 +1,22 @@
+
+import java.util.Map;
+import java.util.HashMap;
+
+
+public class StateCapitals {
+    
+    public static void main (String[] args){
+    
+    Map <String, String> countries= new HashMap<>();
+    
+    countries.put("Alabama", "Montgomery");
+    countries.put("Alaska", "Juneou");
+    countries.put("Arizona", "Phoenix");
+    countries.put("Arkansas", "Little Rock");
+    
+        for(String i: countries.keySet()){
+        System.out.println(i + "_" + countries.get(i));
+
+        }
+    }       
+}
