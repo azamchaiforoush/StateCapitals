1c1dd8387ef120f1fb83cc13b848a1d129522891
azamchaiforoush <84159966+azamchaiforoush@users.noreply.github.com>
2021-07-27 16:37:01 -0700

    stateAndCapitals in NetBeans

---
+++ src/main/java/com/mycompany/statecapitals/StateCapital2.java
@@ -0,0 +1,75 @@
+
+package com.mycompany.statecapitals;
+
+/**
+ *
+ * @author azamchaiforoush
+ */
+import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
+
+
+public class StateCapital2 {
+    public static void main(String[] args){
+        
+     Scanner sc = new Scanner(System.in);
+     Map <String, String> statesAndCapitals = new Hashmap <String ,String>();
+        
+     statesAndCapitals.put("Alabama","Montgomery");
+     statesAndCapitals.put("Alaska","Juneaukey");
+     statesAndCapitals.put("Arizona","Phoenix");
+     statesAndCapitals.put("Arkansas","Little Rock");
+     statesAndCapitals.put("California","Sacramento");
+     statesAndCapitals.put("Colorado","Denver");
+     statesAndCapitals.put("Connecticut","Hartford");
+     statesAndCapitals.put("Delaware","Dover");
+     statesAndCapitals.put("Florida","Tallahassee");
+     statesAndCapitals.put("Georgia","Atlanta");
+     statesAndCapitals.put("Hawaii","Honolulu");
+     statesAndCapitals.put("Idaho","Boise");
+     statesAndCapitals.put("Illinois","Springfield");
+     statesAndCapitals.put("Indiana","Indianapolis");
+     statesAndCapitals.put("Iowa","Des Moines");
+     statesAndCapitals.put("Kansas","Topeka");
+     statesAndCapitals.put("Kentucky","Frankfort");
+     statesAndCapitals.put("Louisiana","Baton Rouge");
+     statesAndCapitals.put("Maine","Augusta");
+     statesAndCapitals.put("Maryland","Annapolis");
+     statesAndCapitals.put("Massachusetts","Boston");
+     statesAndCapitals.put("Michigan","Lansing");
+     statesAndCapitals.put("Minnesota","Saint Paul");
+     statesAndCapitals.put("Mississippi","Jackson");
+     statesAndCapitals.put("Missouri","Jefferson City");
+     statesAndCapitals.put("Montana","Helena");
+     statesAndCapitals.put("Nebraska","Lincoln");
+     statesAndCapitals.put("Nevada","Carson City");
+     statesAndCapitals.put("New Hampshire","Concord");
+     statesAndCapitals.put("New Jersey","Trenton");
+     statesAndCapitals.put("New Mexico","Santa Fe");
+     statesAndCapitals.put("New York","Albany");
+     statesAndCapitals.put("North Carolina","Raleigh");
+     statesAndCapitals.put("North Dakota","Bismarck");
+     statesAndCapitals.put("Ohio","Columbus");
+     statesAndCapitals.put("Oklahoma","Oklahoma City");
+     statesAndCapitals.put("Oregon","Salem");
+     statesAndCapitals.put("Pennsylvania","Harrisburg");
+     statesAndCapitals.put("Rhode Island","Providence");
+     statesAndCapitals.put("South Carolina","Columbia");
+     statesAndCapitals.put("South Dakota","Pierre");
+     statesAndCapitals.put("Tennessee","Nashville");
+     statesAndCapitals.put("Texas","Austin");
+     statesAndCapitals.put("Utah","Salt Lake City");
+     statesAndCapitals.put("Vermont","Montpelier");
+     statesAndCapitals.put("Virginia","Richmond");
+     statesAndCapitals.put("Washington","Olympia");
+     statesAndCapitals.put("West Virginia","Charleston");
+     statesAndCapitals.put("Wisconsin","Madison");
+     statesAndCapitals.put("tWyoming","Cheyenne ");
+        
+     System .out .println();
+        
+        
+    }
+    
+}
